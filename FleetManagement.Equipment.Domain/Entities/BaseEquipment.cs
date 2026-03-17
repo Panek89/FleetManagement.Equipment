6dc54daaf20966064bac1c8b2c4e8e803b9a2e54
Panek89 <pankowski.kamil1989@gmail.com>
2026-03-17 19:46:48 +0100

    WIP: BaseEquipment with derived constrution handling, methods to manipulate

--- FleetManagement.Equipment.Domain/Entities/BaseEquipment.cs
+++ FleetManagement.Equipment.Domain/Entities/BaseEquipment.cs
@@ -5,6 +5,44 @@ namespace FleetManagement.Equipment.Domain.Entities;
 public class BaseEquipment<T> : BaseEntity<T> where T : BaseEquipment<T>
 {
   public Money InitialValue { get; set; }
-  public required string Title { get; set; }
-  public required string Description { get; set; }
+  public Money CurrentValue { get; set; }
+  public string Title { get; set; }
+  public string Description { get; set; }
+
+  public BaseEquipment(Money initialValue, Money currentValue, string title, string description)
+    : base()
+  {
+    InitialValue = initialValue;
+    CurrentValue = currentValue;
+    Title = title;
+    Description = description;
+  }
+
+  public BaseEquipment(bool isActive, string createdBy, Money initialValue, Money currentValue, string title, string description)
+   : base(isActive, createdBy)
+  {
+    InitialValue = initialValue;
+    CurrentValue = currentValue;
+    Title = title;
+    Description = description;
+  }
+
+  public void DecreaseValueByPercentage(decimal percentage)
+  {
+    if (percentage is < 0 or > 100)
+      throw new ArgumentOutOfRangeException(nameof(percentage));
+
+    var amount = CurrentValue.Amount * percentage / 100m;
+    DecreaseValueByAmount(amount);
+  }
+
+  public void DecreaseValueByAmount(decimal amount)
+  {
+    CurrentValue = new Money(CurrentValue.Amount - amount, CurrentValue.Currency);
+  }
+
+  public void Revaluate(decimal value)
+  {
+    CurrentValue = new Money(value, CurrentValue.Currency);
+  }
 }
