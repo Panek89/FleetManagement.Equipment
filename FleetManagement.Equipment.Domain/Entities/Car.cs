bae6d2ca314f845a4ebe0ff54fceb0ec6aed9dae
Panek89 <pankowski.kamil1989@gmail.com>
2026-03-18 22:54:19 +0100

    WIP: Implement first EquipmentType - Car

---
+++ FleetManagement.Equipment.Domain/Entities/Car.cs
@@ -0,0 +1,27 @@
+using System;
+using FleetManagement.Equipment.Domain.ValueObjects;
+
+namespace FleetManagement.Equipment.Domain.Entities;
+
+public sealed class Car : BaseEquipment<Car>
+{
+  public Manufacturer Manufacturer { get; set; }
+  public decimal Mileage { get; set; }
+  public int ProductionYear { get; set; }
+
+  public Car(
+    Money initialValue,
+    Money currentValue,
+    string title,
+    string description,
+    Manufacturer manufacturer,
+    decimal mileage,
+    int productionYear
+  )
+    : base(initialValue, currentValue, title, description)
+  {
+    Manufacturer = manufacturer;
+    Mileage = mileage;
+    ProductionYear = productionYear;
+  }
+}
