0cf94ddfb24fb8b2aaeb51556ae09a870188198f
Panek89 <pankowski.kamil1989@gmail.com>
2026-03-19 22:21:06 +0100

    WIP: Prepare settings for DB connection string, set DbContext in API

--- FleetManagement.Equipment.API/Program.cs
+++ FleetManagement.Equipment.API/Program.cs
@@ -1,17 +1,22 @@
+using FleetManagement.Equipment.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+  opt.UseSqlServer(builder.Configuration.GetConnectionString("SqlDatabase"));
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+  app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
