539e9c94d3ab41977a3d4e8be513ceb6d53e3693
Panek89 <pankowski.kamil1989@gmail.com>
2026-03-19 22:16:47 +0100

    WIP: Empty constructions for handling EF Core DbSets

--- FleetManagement.Equipment.Domain/Entities/Car.cs
+++ FleetManagement.Equipment.Domain/Entities/Car.cs
@@ -5,9 +5,11 @@ namespace FleetManagement.Equipment.Domain.Entities;
 
 public sealed class Car : BaseEquipment<Car>
 {
-  public Manufacturer Manufacturer { get; set; }
+  public Manufacturer Manufacturer { get; set; } = null!;
   public decimal Mileage { get; set; }
   public int ProductionYear { get; set; }
+
+  private Car() { }
 
   public Car(
     Money initialValue,
