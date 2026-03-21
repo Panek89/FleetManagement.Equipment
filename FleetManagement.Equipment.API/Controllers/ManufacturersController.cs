90c56e309dee7a2879bc149c42abc21dcee53cb6
Panek89 <pankowski.kamil1989@gmail.com>
2026-03-21 11:03:28 +0100

    WIP: Scaffold empty controllers

---
+++ FleetManagement.Equipment.API/Controllers/ManufacturersController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FleetManagement.Equipment.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManufacturersController : ControllerBase
+    {
+    }
+}
