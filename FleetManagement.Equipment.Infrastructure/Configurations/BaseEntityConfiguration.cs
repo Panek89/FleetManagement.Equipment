97875b49d9edf0faf719af2840e674e9641580cb
Panek89 <pankowski.kamil1989@gmail.com>
2026-03-21 11:04:07 +0100

    WIP: Adjust DB Configurations

--- FleetManagement.Equipment.Infrastructure/Configurations/BaseEntityConfiguration.cs
+++ FleetManagement.Equipment.Infrastructure/Configurations/BaseEntityConfiguration.cs
@@ -1,4 +1,5 @@
 using FleetManagement.Equipment.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace FleetManagement.Equipment.Infrastructure.Configurations;
@@ -8,5 +9,12 @@ public class BaseEntityConfiguration<T> where T : BaseEntity
   public virtual void Configure(EntityTypeBuilder<T> builder)
   {
     builder.HasKey(x => x.Id);
+
+    builder.Property(p => p.Id).HasColumnName("ID");
+    builder.Property(p => p.IsActive).HasColumnName("IS_ACTIVE");
+    builder.Property(p => p.CreatedAt).HasColumnName("CREATED_AT");
+    builder.Property(p => p.CreatedBy).HasColumnName("CREATED_BY");
+    builder.Property(p => p.UpdatedAt).HasColumnName("UPDATED_AT");
+    builder.Property(p => p.UpdatedBy).HasColumnName("UPDATED_BY");
   }
 }
