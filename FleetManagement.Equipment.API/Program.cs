50adb56f214444e06db5145bac7ef31feb45ee21
Panek89 <pankowski.kamil1989@gmail.com>
2026-03-24 21:04:23 +0100

    WIP: Register Database via infrastructure layer and make sure that connection string to sql server is provided

--- FleetManagement.Equipment.API/Program.cs
+++ FleetManagement.Equipment.API/Program.cs
@@ -3,13 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var sqlConnectionString = builder.Configuration.GetConnectionString("SqlDatabase");
+if (string.IsNullOrWhiteSpace(sqlConnectionString))
+  throw new ArgumentNullException(nameof(sqlConnectionString));
+
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
-
-builder.Services.AddDbContext<AppDbContext>(opt =>
-{
-  opt.UseSqlServer(builder.Configuration.GetConnectionString("SqlDatabase"));
-});
+builder.Services.AddDatabase(sqlConnectionString);
 
 var app = builder.Build();
 
