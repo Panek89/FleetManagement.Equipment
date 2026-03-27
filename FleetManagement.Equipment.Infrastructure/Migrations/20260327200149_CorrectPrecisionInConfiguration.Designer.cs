6e2caac6c1b44337756b22624176f8a1f542fb8e
Panek89 <pankowski.kamil1989@gmail.com>
2026-03-27 21:10:29 +0100

    WIP: Update precision in Money Value Object

--- FleetManagement.Equipment.Infrastructure/Migrations/20260327200149_CorrectPrecisionInConfiguration.Designer.cs
+++ FleetManagement.Equipment.Infrastructure/Migrations/20260327200149_CorrectPrecisionInConfiguration.Designer.cs
@@ -4,6 +4,7 @@ using FleetManagement.Equipment.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FleetManagement.Equipment.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260327200149_CorrectPrecisionInConfiguration")]
+    partial class CorrectPrecisionInConfiguration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FleetManagement.Equipment.Infrastructure/Migrations/20260327200149_CorrectPrecisionInConfiguration.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FleetManagement.Equipment.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorrectPrecisionInConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "INITIAL_VALUE_AMOUNT",
+                table: "CARS",
+                type: "decimal(18,2)",
+                precision: 18,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(2,0)",
+                oldPrecision: 2);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CURRENT_VALUE_AMOUNT",
+                table: "CARS",
+                type: "decimal(18,2)",
+                precision: 18,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(2,0)",
+                oldPrecision: 2);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "INITIAL_VALUE_AMOUNT",
+                table: "CARS",
+                type: "decimal(2,0)",
+                precision: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldPrecision: 18,
+                oldScale: 2);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CURRENT_VALUE_AMOUNT",
+                table: "CARS",
+                type: "decimal(2,0)",
+                precision: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldPrecision: 18,
+                oldScale: 2);
+        }
+    }
+}
