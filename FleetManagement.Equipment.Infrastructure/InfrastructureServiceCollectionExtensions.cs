3b85806d2f8d125f24625ff2133014004c10e523
Panek89 <pankowski.kamil1989@gmail.com>
2026-03-29 19:14:29 +0200

    WIP: Create and register Repository for Manufacturers

--- FleetManagement.Equipment.Infrastructure/InfrastructureServiceCollectionExtensions.cs
+++ FleetManagement.Equipment.Infrastructure/InfrastructureServiceCollectionExtensions.cs
@@ -11,6 +11,7 @@ public static class InfrastructureServiceCollectionExtensions
   {
     // repositories
     services.AddScoped<ICarsRepository, CarsRepository>();
+    services.AddScoped<IManufacturersRepository, ManufacturersRepository>();
 
     return services;
   }
