a6c297923445b9aef66406f7d2350547576c9730
Panek89 <pankowski.kamil1989@gmail.com>
2026-03-31 18:34:14 +0200

    WIP: Manufacturer Controller route path for Get By ID

--- FleetManagement.Equipment.API/Controllers/ManufacturersController.cs
+++ FleetManagement.Equipment.API/Controllers/ManufacturersController.cs
@@ -1,11 +1,25 @@
-using Microsoft.AspNetCore.Http;
+using FleetManagement.Equipment.Application.Manufacturers.Queries;
+using FleetManagement.Equipment.Domain.Entities;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FleetManagement.Equipment.API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ManufacturersController : ControllerBase
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ManufacturersController : ControllerBase
+  {
+    private readonly ISender _sender;
+
+    public ManufacturersController(ISender sender)
     {
+      _sender = sender ?? throw new ArgumentNullException(nameof(sender));
     }
+
+    [HttpGet("get-by-id/{id:guid}")]
+    public async Task<ActionResult<Manufacturer?>> GetById([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+      return await _sender.Send(new GetManufacturerByIdQuery(id), cancellationToken);
+    }
+  }
 }
