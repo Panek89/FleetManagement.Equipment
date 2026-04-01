8b5b69b3e0ac3aadd2cc8a01b8d8ca5be80efc38
Panek89 <pankowski.kamil1989@gmail.com>
2026-04-01 19:51:27 +0200

    WIP: ManufacturerByIdQuery rename and adjust Controller with response

--- FleetManagement.Equipment.Application/Manufacturers/Queries/ManufacturerByIdQueryHandler.cs
+++ FleetManagement.Equipment.Application/Manufacturers/Queries/ManufacturerByIdQueryHandler.cs
@@ -5,18 +5,18 @@ using MediatR;
 
 namespace FleetManagement.Equipment.Application.Manufacturers.Queries;
 
-public record GetManufacturerByIdQuery(Guid Id) : IRequest<ManufacturerDto?>;
+public record ManufacturerByIdQuery(Guid Id) : IRequest<ManufacturerDto?>;
 
-public class GetManufacturerByIdQueryHandler : IRequestHandler<GetManufacturerByIdQuery, ManufacturerDto?>
+public class ManufacturerByIdQueryHandler : IRequestHandler<ManufacturerByIdQuery, ManufacturerDto?>
 {
   private readonly IManufacturersRepository _manufacturersRepository;
 
-  public GetManufacturerByIdQueryHandler(IManufacturersRepository manufacturersRepository)
+  public ManufacturerByIdQueryHandler(IManufacturersRepository manufacturersRepository)
   {
     _manufacturersRepository = manufacturersRepository ?? throw new ArgumentNullException(nameof(manufacturersRepository));
   }
 
-  public async Task<ManufacturerDto?> Handle(GetManufacturerByIdQuery query, CancellationToken cancellationToken)
+  public async Task<ManufacturerDto?> Handle(ManufacturerByIdQuery query, CancellationToken cancellationToken)
   {
     var manufacturer = await _manufacturersRepository.GetByIdAsync(query.Id, cancellationToken);
     return manufacturer is not null ? manufacturer.MapToDto() : null;
