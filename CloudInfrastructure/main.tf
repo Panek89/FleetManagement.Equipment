62dd62a813d1fdc123584be24240be8a642f0524
Panek89 <pankowski.kamil1989@gmail.com>
2026-04-21 17:47:49 +0200

    WIP: AppConfiguration scaffold

--- CloudInfrastructure/main.tf
+++ CloudInfrastructure/main.tf
@@ -13,5 +13,12 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "resource_group" {
   name     = "rg-${var.resource-suffix}"
-  location = "West Europe"
+  location = var.location-region
+}
+
+resource "azurerm_app_configuration" "app_configuration" {
+  name                = "appconf-${var.resource-suffix}"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location            = var.location-region
+  sku                 = "free"
 }
