7bcee92abd6d4811a3214939226fa4ed50c89894
Panek89 <pankowski.kamil1989@gmail.com>
2026-04-22 12:07:41 +0200

    WIP: Provision free SQL Server with Database and if it's run locally, add current ip to firewall automatically

---
+++ CloudInfrastructure/mssql_database.tf
@@ -0,0 +1,27 @@
+resource "azurerm_mssql_server" "mssql_server" {
+  name                = "mssql-server-${var.resource-suffix}"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location            = var.location-region
+  version             = "12.0"
+  administrator_login = "sqladmin"
+
+  administrator_login_password = random_password.sql_admin_password.result
+
+  depends_on = [azurerm_app_configuration.app_configuration]
+}
+
+resource "azurerm_mssql_database" "mssql_db_fleetmanagement" {
+  name      = "mssql-db-${var.resource-suffix}"
+  server_id = azurerm_mssql_server.mssql_server.id
+  collation = "SQL_Latin1_General_CP1_CI_AS"
+  sku_name  = "free"
+}
+
+resource "azurerm_mssql_firewall_rule" "allow_my_ip" {
+  count = var.is_local ? 1 : 0
+
+  name             = "AllowLocalDevIP"
+  server_id        = azurerm_mssql_server.mssql_server.id
+  start_ip_address = trimspace(data.http.my_public_ip.response_body)
+  end_ip_address   = trimspace(data.http.my_public_ip.response_body)
+}
