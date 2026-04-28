d10443824fd3162192126fa842b59d2f2d85a253
Panek89 <pankowski.kamil1989@gmail.com>
2026-04-28 17:42:47 +0200

    WIP: Adjust Terraform files to new Infrastructure project

--- CloudInfrastructure/variables.tf
+++ CloudInfrastructure/variables.tf
@@ -1,17 +1,36 @@
-variable "resource-suffix" {
+variable "shared_resource_group_name" {
   type        = string
-  description = "Resource group name"
-  default     = "fleetmanagement"
+  description = "Name of the shared resource group"
 }
 
-variable "location-region" {
+variable "location" {
   type        = string
-  description = "Location region"
-  default     = "West Europe"
+  description = "Azure region for resources"
+  default     = "westeurope"
+}
+
+variable "env_suffix" {
+  type        = string
+  description = "Environment suffix (e.g. dev, prod)"
+}
+
+variable "fm_suffix" {
+  type        = string
+  description = "Suffix for FleetManagement project"
+}
+
+variable "mssql_server_name" {
+  type        = string
+  description = "Name of the existing MSSQL Server"
+}
+
+variable "app_configuration_name" {
+  type        = string
+  description = "Name of the existing App Configuration"
 }
 
 variable "is_local" {
   type        = bool
   default     = false
-  description = "Set to false if you are running your code in Pipeline"
+  description = "Set to true if running from local machine to allow IP in firewall"
 }
